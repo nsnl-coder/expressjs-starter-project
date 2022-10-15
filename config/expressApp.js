47ef0157c25be67e0fdc83160e97c2d69c3befca
nsnl <nsnl.coder@gmail.com>
2022-10-14 22:36:43 -0400

    handler factory added + api features

--- config/expressApp.js
+++ config/expressApp.js
@@ -3,13 +3,15 @@ const express = require('express')
 // Router
 const exampleRouter = require('../routes/exampleRoutes')
 const pageNotFound = require('./../middleware/pageNotFound')
+const globalErrorHandler = require('./../middleware/globalErrorHandler')
 
 const app = express()
 
 // Routers
 app.use('/api/v1', exampleRouter)
-
-// Not found route
 app.use('*', pageNotFound)
+
+// apply global error handler
+app.use(globalErrorHandler)
 
 module.exports = app
