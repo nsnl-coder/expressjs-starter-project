de4a0e9101144492376aa5fcf8fd210f76d2f23a
nsnl <nsnl.coder@gmail.com>
2022-10-20 01:03:11 -0400

    add route restriction

---
+++ middleware/routeRestriction.js
@@ -0,0 +1,17 @@
+const userModel = require('../models/userModel')
+const AppError = require('../utils/AppError')
+const catchAsync = require('../utils/catchAsync')
+
+const routeRestriction = (...authorizedRoles) =>
+  catchAsync(async (req, res, next) => {
+    const user = await userModel.findOne(req.user._id).select('+role')
+    console.log(user)
+
+    const istrue = authorizedRoles.includes(user.role)
+    if (!authorizedRoles.includes(user.role)) {
+      return next(new AppError('You are not authorized to perform this action.', 404))
+    }
+    next()
+  })
+
+module.exports = routeRestriction
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const authController = require('../controllers/authController')
 const userController = require('../controllers/userController')
 const addUserIdToParams = require('../middleware/addUserIdToParams')
 const routeProtect = require('../middleware/routeProtect')
+const routeRestriction = require('../middleware/routeRestriction')
 const validateUser = require('../validation/user/index')
 
 const Router = express.Router()
@@ -16,17 +17,17 @@ Router.patch('/resetPassword/:token', authController.resetPassword)
 
 // protected routed
 Router.use(routeProtect)
-Router.route('/')
-  .delete(userController.deleteManyUser)
-  .get(userController.getAllUsers)
-  .put(userController.updateManyUsers)
 
-// Router.use(addUserIdToParams)
 Router.patch('/updatePassword', authController.updatePassword)
-
 Router.route('/profile')
   .patch(addUserIdToParams, validateUser.onUpdate, userController.updateUser)
   .get(addUserIdToParams, userController.getUser)
   .delete(addUserIdToParams, userController.deleteUser)
 
+// restrict role to admin
+Router.use(routeRestriction('admin', 'ahah'))
+Router.route('/')
+  .delete(userController.deleteManyUser)
+  .get(userController.getAllUsers)
+  .put(userController.updateManyUsers)
 module.exports = Router
